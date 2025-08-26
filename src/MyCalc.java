ee856668440ca4f440105d2004ab311975bc8a59
XNikhil44 <xnikhilkumar44@gmail.com>
2025-08-26 11:14:17 +0530

    26 Aug 2025

---
+++ src/MyCalc.java
@@ -0,0 +1,8 @@
+public class MyCalc {
+    public static void main(String[] args) {
+        Calculator cl=new Calculator();
+        cl.Area(12.3);
+        cl.Area(2,4);
+        cl.Area(3,5,6);
+    }
+}
---
+++ src/MyPerson.java
@@ -0,0 +1,8 @@
+public class MyPerson {
+    public static void main(String[] args) {
+        Person p=new Person("Ghaziabad");
+        p.getAddress();
+    }
+}
+
+
