6f046dc39c9d1004e5cbadf34c82eb9bef6c5be5
XNikhil44 <xnikhilkumar44@gmail.com>
2025-08-29 11:45:45 +0530

    29 Aug 2025 (User Defined Exception)

---
+++ src/MyBank.java
@@ -0,0 +1,16 @@
+import java.awt.image.BandCombineOp;
+
+public class MyBank {
+    public static void main(String[] args) {
+        Bank b=new Bank();
+
+        try {
+            b.withdrawal(12000);
+        }
+        catch (InsufficientBalanceException e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
